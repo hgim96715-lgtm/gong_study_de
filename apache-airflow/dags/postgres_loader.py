fa59e73363bd38144d3871d389607e43eed849fe
hgim96715-lgtm <hgim96715-lgtm@gmail.com>
2026-01-22 16:00:35 +0900

    sensor+

--- apache-airflow/dags/postgres_loader.py
+++ apache-airflow/dags/postgres_loader.py
@@ -1,14 +1,14 @@
 from airflow import DAG
 # 기존 PostgresOperator 대신 SQLExecuteQueryOperator 사용(범용)
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.providers.common.sql.sensors.sql import SqlSensor
 from datetime import datetime
 
 with DAG(
     dag_id="postgres_loader",
     start_date=datetime(2026, 1, 1),
     schedule=None,
-    catchup=False,
-) as dag:
+    catchup=False) as dag:
 
         
     create_table = SQLExecuteQueryOperator(
@@ -29,8 +29,24 @@ with DAG(
         conn_id="my_postgres_connection",
         sql="""
         INSERT INTO sample_table (key, value)
-        VALUES ('hello', 'world');
+        VALUES ('my', 'connection');
         """
     )
+    sql_sensor=SqlSensor(
+        task_id='wait_for_condition',
+        conn_id='my_postgres_connection',
+        sql="SELECT COUNT(*) FROM sample_table WHERE key='my'",
+        mode='poke',
+        poke_interval=30
+    )
+    sql_query_confirm="""
+        INSERT INTO sample_table(key,value)
+        VALUES('sensor','confirmed');
+    """
+    postres_confirm_task=SQLExecuteQueryOperator(
+        task_id='execute_sql_confirm_query',
+        conn_id='my_postgres_connection',
+        sql=sql_query_confirm
+    )
 
-    create_table >> insert_data
+    create_table >> insert_data >> sql_sensor >> postres_confirm_task
