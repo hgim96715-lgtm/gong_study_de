f954227e45ffee85a5c2044883b4e57b458f744c
hgim96715-lgtm <hgim96715-lgtm@gmail.com>
2026-01-22 22:48:53 +0900

    schedule+

---
+++ apache-airflow/dags/my_consumer.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from airflow.datasets import Dataset
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+local_file = Dataset("/tmp/sample.txt")
+
+with DAG(
+    dag_id='my_consumer',
+    start_date=datetime(2023, 7, 1),
+    schedule=[local_file],
+    catchup=False) as dag:
+    
+    task_consumer = BashOperator(
+        task_id="consumer1",
+        bash_command='cat /tmp/sample.txt',
+    )
