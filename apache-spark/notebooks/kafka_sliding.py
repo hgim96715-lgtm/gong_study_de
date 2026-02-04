f4c0c0e23a3c26ad0f34a742c281907d32c4b5c5
hgim96715-lgtm <hgim96715@gmail.com>
2026-02-04 15:09:51 +0900

    kafka_time_sliding_tumbling

---
+++ apache-spark/notebooks/kafka_sliding.py
@@ -0,0 +1,71 @@
+"""
+{"create_date":"2026-02-04 00:07:00","amount": 1000}
+"""
+
+
+from pyspark.sql import SparkSession
+from pyspark.sql.types import  StructType, StructField, StringType, IntegerType
+from pyspark.sql.functions import from_json, col, to_timestamp, window,sum
+
+spark = (
+    SparkSession
+    .builder
+    .master("spark://spark-master:7077")  
+    .appName("kafkaTumblingTime")             
+    .config("spark.streaming.stopGracefullyOnShutdown", "true")
+    .config("spark.sql.shuffle.partitions", "3") 
+    .getOrCreate()
+)
+
+schema = StructType([
+    StructField("create_date", StringType()),
+    StructField("amount", IntegerType())
+])
+
+events = (
+    spark
+    .readStream
+    .format("kafka")
+    .option("kafka.bootstrap.servers", "kafka:9092")
+    .option("subscribe", "timeseries")        
+    .option("startingOffsets", "earliest")
+    .option("failOnDataLoss", "false")
+    .load()
+)
+
+value_df = events.select(
+    col('key'),
+    from_json(col("value").cast("string"), schema).alias("value")
+)
+timestamp_format="yyyy-MM-dd HH:mm:ss"
+tf_df=(
+    value_df
+    .select("value.*")
+    .withColumn("create_date",to_timestamp("create_date",timestamp_format))
+)
+
+window_duration="10 minutes"
+slide_duration="5 minutes"
+window_agg_df = (
+    tf_df
+    .groupBy(window(col("create_date"),window_duration,slide_duration))               
+    .agg(sum("amount").alias("total_amount"))
+)
+
+query=(
+    window_agg_df
+    .writeStream
+    .outputMode("update")
+    .format("console")
+    .option("truncate","false")
+    .trigger(processingTime="5 seconds")
+    .start()
+)
+query.awaitTermination()
+
+
+
+
+
+
+
