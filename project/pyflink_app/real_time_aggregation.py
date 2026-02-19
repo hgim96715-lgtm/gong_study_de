cbd8f6e0da9ae98ebf84c4085cfd570ed0070df5
hgim96715-lgtm <hgim96715@gmail.com>
2026-02-20 00:27:50 +0900

    [Project] PyFlink Kafka 카테고리별 1분 매출 집계 파이프라인 추가

---
+++ project/pyflink_app/real_time_aggregation.py
@@ -0,0 +1,58 @@
+from pyflink.table import EnvironmentSettings,TableEnvironment
+from pyflink.table.window import Tumble
+from pyflink.table.expressions import col,lit
+
+def real_time_aggregation():
+    settings=EnvironmentSettings.in_streaming_mode()
+    t_env=TableEnvironment.create(settings)
+    
+    print(">>> PyFlink Start!!")
+    
+    t_env.execute_sql("""
+                      CREATE TABLE source_table(
+                          order_id STRING,
+                          total_amount BIGINT,
+                          category STRING,
+                          
+                          `timestamp` TIMESTAMP(3),
+                          
+                          WATERMARK FOR `timestamp` AS `timestamp` - INTERVAL '5' SECOND
+                          ) WITH(
+                              'connector'='kafka',
+                              'topic'='user-log',
+                              'properties.bootstrap.servers'='kafka:9092',
+                              'properties.group.id'='project-flik-aggregator-dev',
+                              'scan.startup.mode'='latest-offset',
+                              'format'='json',
+                              'json.timestamp-format.standard' = 'ISO-8601'
+                              )""")
+    
+    agg_query="""
+        SELECT
+            window_start,
+            window_end,
+            category,
+            SUM(total_amount) as total_sales,
+            COUNT(order_id) as total_orders
+        FROM TABLE(
+            TUMBLE(TABLE source_table,DESCRIPTOR(`timestamp`),INTERVAL '1' MINUTE)
+        )
+        GROUP BY window_start,window_end,category"""
+        
+    
+    t_env.execute_sql("""
+                      CREATE TABLE print_sink(
+                          window_start TIMESTAMP(3),
+                          window_end TIMESTAMP(3),
+                          category STRING,
+                          total_sales BIGINT,
+                          total_orders BIGINT
+                      ) WITH(
+                          'connector'='print'
+                      )
+                """)
+    
+    t_env.execute_sql(f"INSERT INTO print_sink {agg_query}")
+    
+if __name__=='__main__':
+    real_time_aggregation()
